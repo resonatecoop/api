4feb6e0d81bb6df0c4643269bf9ec3bbab48b21c
Si <simsamsomred@gmail.com>
2022-09-30 15:39:06 -0400

    chore: uncomment certbot for now

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-certbot certonly --webroot --webroot-path=/var/www/html -d dev.resonate.coop --email dev@resonate.coop -n --agree-tos --expand
+# certbot certonly --webroot --webroot-path=/var/www/html -d dev.resonate.coop --email dev@resonate.coop -n --agree-tos --expand
 
-/usr/sbin/nginx -g "daemon off;" # start nginx in the background
+# /usr/sbin/nginx -g "daemon off;" # start nginx in the background
