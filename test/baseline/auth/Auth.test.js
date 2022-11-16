008e0358b1f11063f9f7327b19987e8fffbbd5f1
Si <simsamsomred@gmail.com>
2022-11-16 06:22:53 -0500

    test: add basic tests for password reset

--- test/baseline/auth/Auth.test.js
+++ test/baseline/auth/Auth.test.js
@@ -4,6 +4,7 @@
 const baseURL = `${process.env.APP_HOST}`
 const request = require('supertest')(baseURL)
 const { expect } = require('../../testConfig')
+const { User } = require('../../../src/db/models')
 
 const ResetDB = require('../../ResetDB')
 
@@ -22,5 +23,38 @@ describe('Auth endpoint test', () => {
       .type('form')
 
     expect(response.status).to.eql(200)
+  })
+
+  it('should handle password reset', async () => {
+    const user = await User.create({
+      email: faker.internet.email(),
+      password: 'blabla',
+      roleId: 1
+    })
+    response = await request.post('/password-reset')
+      .send({
+        email: user.email
+      })
+      .type('form')
+
+    expect(response.status).to.eql(200)
+    expect(response.text).to.include('Password reset email sent')
+
+    await user.reload()
+    expect(user.emailConfirmationToken).not.to.be.null
+    expect(user.emailConfirmationExpiration).not.to.be.null
+
+    await user.destroy({ force: true })
+  })
+
+  it('should handle password reset with non-existent password', async () => {
+    response = await request.post('/password-reset')
+      .send({
+        email: faker.internet.email()
+      })
+      .type('form')
+
+    expect(response.status).to.eql(200)
+    expect(response.text).to.include('No user with this email exists')
   })
 })
