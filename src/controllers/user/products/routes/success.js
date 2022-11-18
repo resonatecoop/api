5566a0e62a311e5d41fdc51b06f33e1cdd1c791b
Si <simsamsomred@gmail.com>
2022-11-18 16:35:13 -0500

    feat: transition stripe success updates to happen via a webhook

--- src/controllers/user/products/routes/success.js
+++ src/controllers/user/products/routes/success.js
@@ -1,13 +1,3 @@
-const { Credit, User } = require('../../../../db/models')
-
-const stripe = require('stripe')(process.env.STRIPE_KEY)
-
-const values = {
-  'Stream-Credit-05': 5,
-  'Stream-Credit-10': 10,
-  'Stream-Credit-20': 20,
-  'Stream-Credit-50': 50
-}
 
 module.exports = function () {
   const operations = {
@@ -16,23 +6,7 @@ module.exports = function () {
 
   async function GET (ctx, next) {
     try {
-      // FIXME: Better error handling here
       const response = ctx.request.query
-      const session = await stripe.checkout.sessions.retrieve(ctx.request.query.session_id)
-      const lineItems = await stripe.checkout.sessions.listLineItems(session.id)
-      const user = await User.findOne({
-        where: { id: response.userId }
-      })
-
-      await Promise.all(lineItems.data.map(async lineItem => {
-        if (lineItem.description.includes('Stream-Credit')) {
-          const [credit] = await Credit.findOrCreate({
-            where: { user_id: user.id }
-          })
-          await credit.update({ total: credit.total + values[lineItem.description] })
-          await credit.save()
-        }
-      }))
 
       ctx.status = 303
       ctx.redirect(response.callbackURL)
@@ -43,36 +17,6 @@ module.exports = function () {
     }
 
     await next()
-  }
-
-  GET.apiDoc = {
-    operationId: 'checkoutSuccess',
-    description: 'Checkout success',
-    summary: 'Checkout success',
-    tags: ['products'],
-    produces: [
-      'application/json'
-    ],
-    responses: {
-      400: {
-        description: 'Bad request',
-        schema: {
-          $ref: '#/responses/BadRequest'
-        }
-      },
-      404: {
-        description: 'Not found',
-        schema: {
-          $ref: '#/responses/NotFound'
-        }
-      },
-      default: {
-        description: 'error payload',
-        schema: {
-          $ref: '#/definitions/Error'
-        }
-      }
-    }
   }
 
   return operations
