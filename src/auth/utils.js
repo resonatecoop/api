350992f981914a67f49e815c7cb714f11142f695
Si <simsamsomred@gmail.com>
2022-11-19 12:13:34 -0500

    fix: add logout page styles. closes #151

--- src/auth/utils.js
+++ src/auth/utils.js
@@ -8,3 +8,24 @@ module.exports.renderError = (ctx, out, error) => {
     }
   })
 }
+
+module.exports.logoutSource = (ctx, form) => {
+  ctx.type = 'html'
+  return ctx.render('logout', {
+    form,
+    host: ctx.host
+  })
+}
+
+module.exports.postLogoutSuccessSource = async (ctx) => {
+  ctx.body = `<!DOCTYPE html>
+    <head>
+      <title>Signed out</title>
+    </head>
+    <body>
+      <script>
+        window.close()
+      </script>
+    </body>
+    </html>`
+}
